e5e7c80ed25a4d9b954a732127ebb833d48c1936
MColombo00 <48252195+MColombo00@users.noreply.github.com>
2025-07-23 21:38:51 -0400

    video

--- CameraVideoTest.py
+++ CameraVideoTest.py
@@ -6,8 +6,8 @@ dt = datetime.datetime.now().strftime("%m-%d-%y_%I%M%S")
 os.system("mkdir /home/cam/Desktop/Camera_Tests/" + dt)
 
 
-os.system("libcamera-still --camera 1 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast -o /home/cam/Desktop/Camera_Tests/" + dt + "/C1-" + dt + "&"
-)
+os.system("libcamera-vid --camera 0 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast -o /home/cam/Desktop/Camera_Tests/" + dt + "/C0-" + dt + "&" +
+"libcamera-vid --camera 1 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast -o /home/cam/Desktop/Camera_Tests/" + dt + "/C1-" + dt)
 
 print("Done!")
 
