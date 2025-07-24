692beca8b258efd0cd3fd4a05ad0f80558e2f9f9
MColombo00 <48252195+MColombo00@users.noreply.github.com>
2025-07-24 10:20:11 -0400

    integrated circuit board gpio useage

--- CameraVideoTest.py
+++ CameraVideoTest.py
@@ -1,14 +1,32 @@
 import os, datetime, multiprocessing
+import gpiozero as GPIO
 
-dt = datetime.datetime.now().strftime("%m-%d-%y_%I%M%S")
-#print(dt)a
+red = GPIO.LED(14)
+blue = GPIO.LED(15)
+green = GPIO.LED(18)
+sw = GPIO.Button(17)
 
-os.system("mkdir /home/cam/Desktop/Camera_Tests/" + dt)
+while True:
+    green.on()
 
+    sw.wait_for_press()
+    green.off()
+    red.blink(on_time=0.5, off_time=0.5, background=True)
 
-os.system("libcamera-vid --camera 0 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast -o /home/cam/Desktop/Camera_Tests/" + dt + "/C0-" + dt + "&" +
-"libcamera-vid --camera 1 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast -o /home/cam/Desktop/Camera_Tests/" + dt + "/C1-" + dt)
+    dt = datetime.datetime.now().strftime("%m-%d-%y_%I%M%S")
+    #print(dt)a
 
-print("Done!")
+    os.system("mkdir /home/cam/Desktop/Camera_Tests/" + dt)
+
+    os.system(
+    "libcamera-vid --codec libav --camera 0 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast -o /home/cam/Desktop/Camera_Tests/" + dt + "/C0-" + dt + ".mp4" + "&" +
+    "libcamera-vid --codec libav --camera 1 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast -o /home/cam/Desktop/Camera_Tests/" + dt + "/C1-" + dt +".mp4")
+
+    red.off()
+    blue.blink(on_time=0.2, off_time=0.2,n=5, background=False)
+    print("Done!")
+
+    blue.off()
+
 
 exit()
