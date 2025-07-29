aad4b50e375b8af8565bb303b49e272a8df061e6
MColombo00 <48252195+MColombo00@users.noreply.github.com>
2025-07-28 20:00:22 -0400

    added timelapse function for testing to see which is better

--- CameraVideoTest.py
+++ CameraVideoTest.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import os, datetime, multiprocessing
 import gpiozero as GPIO
 
@@ -16,11 +17,23 @@ while True:
     dt = datetime.datetime.now().strftime("%m-%d-%y_%I%M%S")
     #print(dt)aa
 
-    os.system("mkdir /home/cam/Desktop/Camera_Tests/" + dt)
+#     os.system("mkdir /home/cam/Desktop/Camera_Tests/" + dt)
+    os.system("mkdir /home/cam/Desktop/timelapseTest/" + dt)
+
+#	Video Test
+#     os.system(
+#     "rpicam-vid --codec libav --camera 0 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast --width 4656 --height 3496 -o /home/cam/Desktop/Camera_Tests/" + dt + "/C0-" + dt + ".mp4" + "&" +
+#     "rpicam-vid --codec libav --camera 1 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast --width 4656 --height 3496 -o /home/cam/Desktop/Camera_Tests/" + dt + "/C1-" + dt +".mp4")
 
     os.system(
-    "libcamera-vid --codec libav --camera 0 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast -o /home/cam/Desktop/Camera_Tests/" + dt + "/C0-" + dt + ".mp4" + "&" +
-    "libcamera-vid --codec libav --camera 1 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast -o /home/cam/Desktop/Camera_Tests/" + dt + "/C1-" + dt +".mp4")
+    "rpicam-still --timeout 12000 --timelapse 2000 --camera 0 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast --preview 600,0,700,600 -o /home/cam/Desktop/timelapseTest/" + dt + "/C0-%04d.jpg" + "&" +
+    "rpicam-still --timeout 12000 --timelapse 2000 --camera 1 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast --preview 0,0,600,600 -o /home/cam/Desktop/timelapseTest/" + dt + "/C1-%04d.jpg")
+
+
+#	Hello Test
+#     os.system(
+#         "libcamera-hello --camera 0 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast --framerate 60 -o /home/cam/Desktop/Camera_Tests/" + dt + "/C0-" + dt + "" + "&" +
+#         "libcamera-hello --camera 1 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast --framerate 60 -o /home/cam/Desktop/Camera_Tests/" + dt + "/C1-" + dt +"")
 
     red.off()
     blue.blink(on_time=0.2, off_time=0.2,n=5, background=False)
