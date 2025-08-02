4fbbae85eaecda59fef3777d6fd10cf081068a95
MColombo00 <48252195+MColombo00@users.noreply.github.com>
2025-08-01 21:34:26 -0400

    tweaking files, adding metadata to images

--- CameraVideoTest.py
+++ CameraVideoTest.py
@@ -7,43 +7,43 @@ blue = GPIO.LED(15)
 green = GPIO.LED(18)
 sw = GPIO.Button(17)
 
+    
 while True:
     green.on()
 
     sw.wait_for_press()
     green.off()
     red.blink(on_time=0.5, off_time=0.5, background=True)
-
+    
+        
     dt = datetime.datetime.now().strftime("%m-%d-%y_%I%M%S")
+    os.system("mkdir /home/cam/Desktop/timelapseTest/" + dt)
+    
     #print(dt)
 
 #   os.system("mkdir /home/cam/Desktop/Camera_Tests/" + dt)
-    os.system("mkdir /home/cam/Desktop/timelapseTest/" + dt)
+
 
 #	Video Test
 #     os.system(
-#     "rpicam-vid --codec libav --camera 0 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast --width 4656 --height 3496 -o /home/cam/Desktop/Camera_Tests/" + dt + "/C0-" + dt + ".mp4" + "&" +
-#     "rpicam-vid --codec libav --camera 1 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast --width 4656 --height 3496 -o /home/cam/Desktop/Camera_Tests/" + dt + "/C1-" + dt +".mp4")
+#     "rpicam-vid --codec libav --camera 0 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast --width 4656 --height 3496 --preview 600,0,700,600 -o /home/cam/Desktop/Camera_Tests/" + dt + "/C0-" + dt + ".mp4" + "&" +
+#     "rpicam-vid --codec libav --camera 1 -t 10000 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast --width 4656 --height 3496 --preview 0,0,600,600 -o /home/cam/Desktop/Camera_Tests/" + dt + "/C1-" + dt +".mp4")
 
     #Basic timelapse, no image customization
-#     os.system( 
-#     "rpicam-still --timeout 30000 --timelapse 2000 --camera 0 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast --preview 600,0,700,600 -o /home/cam/Desktop/timelapseTest/" + dt + "/C0-%04d.jpg" + "&" +
-#     "rpicam-still --timeout 30000 --timelapse 2000 --camera 1 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast --preview 0,0,600,600 -o /home/cam/Desktop/timelapseTest/" + dt + "/C1-%04d.jpg")
+    os.system( 
+    "rpicam-still --metadata C1-%04d.json --timeout 30000 --timelapse 2000 --camera 1 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast --width 4656 --height 3496 -o /home/cam/Desktop/timelapseTest/" + dt + "/C1-%04d.jpg" + "&" +
+    "rpicam-still --metadata C0-%04d.json --timeout 30000 --timelapse 2000 --camera 0 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast --width 4656 --height 3496 -o /home/cam/Desktop/timelapseTest/" + dt + "/C0-%04d.jpg")
 
 #     os.system( 
-#     "rpicam-still --timeout 30000 --timelapse 2000 --camera 0 --autofocus-on-capture --autofocus-range full --width 4656 --height 3496 --autofocus-speed fast --preview 600,0,700,600 -o /home/cam/Desktop/timelapseTest/" + dt + "/C0-%04d.jpg" + "&" +
-#     "rpicam-still --timeout 30000 --timelapse 2000 --camera 1 --autofocus-on-capture --autofocus-range full --width 4656 --height 3496 --autofocus-speed fast --preview 0,0,600,600 -o /home/cam/Desktop/timelapseTest/" + dt + "/C1-%04d.jpg")
+#     "rpicam-still --timeout 30000 --timelapse 2000 --camera 0 --autofocus-on-capture --autofocus-range full --width 4656 --height 3496 --autofocus-speed fast -n -o /home/cam/Desktop/timelapseTest/" + dt + "/C0-%04d.jpg" + "&" +
+#     "rpicam-still --timeout 30000 --timelapse 2000 --camera 1 --autofocus-on-capture --autofocus-range full --width 4656 --height 3496 --autofocus-speed fast -n -o /home/cam/Desktop/timelapseTest/" + dt + "/C1-%04d.jpg")
 #     
     
-
-    
-
-
     red.off()
-    blue.blink(on_time=0.2, off_time=0.2,n=5, background=False)
+    blue.blink(on_time=0.2,off_time=0.2,n=5, background=False)
     print("Done!")
 
     blue.off()
-
-
+     
+green.off()
 exit()
